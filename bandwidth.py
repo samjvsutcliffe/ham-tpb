4f77385ede6d4aca679debca5e231d671fe50d47
Sam Sutcliffe <sam.sutcliffe@.dur.ac.uk>
2023-12-17 01:22:02 +0000

    Merged changes but use parameteric

---
+++ bandwidth.py
@@ -0,0 +1,116 @@
+import matplotlib as mpl
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.patches import Rectangle
+from matplotlib.collections import PatchCollection
+from matplotlib import cm
+import re
+import os
+import json
+import numpy as np
+import pandas as pd
+import json
+from vtk import vtkUnstructuredGridReader
+from vtk.util import numpy_support as VN
+from vtk.util.numpy_support import vtk_to_numpy, numpy_to_vtk
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.patches import Rectangle
+from matplotlib.collections import PatchCollection
+from matplotlib import cm
+from multiprocessing import Pool
+import os
+import re
+
+NO_OVERWRITE = False
+
+
+def get_data(filename):
+    reader = vtkUnstructuredGridReader()
+    reader.SetFileName(filename)
+    reader.ReadAllVectorsOn()
+    reader.ReadAllScalarsOn()
+    reader.Update()
+
+    data = reader.GetOutput()
+
+    vtk_points = data.GetPoints()
+    xyz3d = vtk_to_numpy( vtk_points.GetData() )
+    xy = xyz3d[:,0:2]
+    scalar_names = [reader.GetScalarsNameInFile(i) for i in range(0, reader.GetNumberOfScalarsInFile())]
+    scalar_data = data.GetPointData()
+    #scalar_names = scalar_data.GetArrayNames()
+    def GetScalar(scalar_name):
+        return vtk_to_numpy(scalar_data.GetArray(scalar_names.index(scalar_name)))
+    lx = GetScalar("size_x")
+    ly = GetScalar("size_y")
+    damage = GetScalar("damage")
+    return pd.DataFrame({"coord_x":xy[:,0], "coord_y":xy[:,1],"lx":lx,"ly":ly,"damage":damage})
+
+
+
+import subprocess
+
+#plt.rc('font', family='serif', serif='Times')
+## plt.rc('text', usetex=True)
+#plt.rc('xtick', labelsize=8)
+#plt.rc('ytick', labelsize=8)
+#plt.rc('axes', labelsize=8)
+#width = 3.487
+#height = width / 1.618
+#
+#output_dir = "../output/"
+#files = os.listdir(output_dir)
+#finalcsv = re.compile("sim.*\.vtk")
+#files_csvs = list(filter(finalcsv.match,files))
+#print("files: {}".format(files_csvs))
+#df = get_data(output_dir+"{}".format(files_csvs[-1]))
+
+def plot(df,cutoff=0):
+    fig = plt.figure()#(figsize=(16,9),dpi=200)
+    ax = fig.add_subplot(111,aspect="equal")
+    patch_list=[]
+    xlim = [0,0.110*2]
+    ylim = [0,0.110]
+    for a_x, a_y,lx,ly,damage in zip(df["coord_x"],
+                                     df["coord_y"],
+                                     df["lx"],
+                                     df["ly"],
+                                     df["damage"]):
+        patch = Rectangle(
+            xy=(a_x-lx/2, a_y-ly/2) ,width=lx, height=ly)
+        patch_list.append(patch)
+    p = PatchCollection(patch_list, cmap=cm.jet, alpha=1)
+    p.set_array(df["damage"])
+    p.set_clim([cutoff,1.0])
+    ax.add_collection(p)
+    fig.colorbar(p,location="bottom",label="damage")
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+
+
+def get_all_data(folder):
+    regex = re.compile(r'sim_final_.*')
+    files = list(filter(regex.search,os.listdir(folder)))
+    print(files)
+    subframes = [get_data(folder + "/" + f) for f in files]
+    df = pd.concat(subframes)
+    return df
+
+
+def measure_damage(df):
+    return df[df["damage"]>0.9]["coord_x"].max()
+
+
+regex = re.compile(r'output-.*')
+files = list(filter(regex.search,os.listdir("./")))
+for f in files:
+    #plt.figure()
+    df = get_all_data(f)
+    plot(df)
+    plt.title(f)
+    print("File:",f)
+    print("Damage zone:",measure_damage(df))
+plt.show()
